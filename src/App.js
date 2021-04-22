3dc98ca33a23b102359569bd5616ce5f3d1100e9
LeKarimDerradji <karim.derradji.pro@gmail.com>
2021-04-22 04:02:07 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,63 @@
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios'
+import {useState, useEffect} from 'react'
+import Coin from './Coin.js'
+import Header from './Header'
+
+const URL_API = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=eur&order=market_cap_desc&per_page=100&page=1&sparkline=false'
 
 function App() {
+
+  const [coins, setCoins] = useState([])
+  const [search, setSearch] = useState('')
+
+  useEffect(() => {
+
+    axios.get(URL_API)
+    .then(res => {
+      setCoins(res.data)
+      console.log(res.data)
+    })
+    .catch(e => console.log(e))
+
+  }, [])
+
+  const handleChange = e => {
+    setSearch(e.target.value)
+  }
+
+
+  const filteredCoins = coins.filter(coin => 
+    coin.name.toLowerCase().includes(search.toLowerCase())
+  )
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="coin-app">
+      <div className="coin-search">
+
+        <h1 className="coin-text">Search a currency</h1>
+
+        <form action="">
+        <input type="text" placeholder='Seach' 
+        name="" 
+        id=""
+        className="coin-input" onChange={handleChange} />
+        </form>
+
+      </div>
+        <Header/>
+       {filteredCoins.map(coin => { // Mapping trought the API_Coin Object, Rendering wanted elements. 
+        return (<Coin 
+          key={coin.id} 
+          name={coin.name} 
+          symbol={coin.symbol} 
+          price={coin.current_price} 
+          image={coin.image}
+          marketCap={coin.market_cap} 
+          priceChange={coin.price_change_percentage_24h}
+          volume={coin.total_volume}/>)
+       })}
     </div>
   );
 }
---
+++ src/Header.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import './Header.css'
+
+const Header = ({}) => {
+    return (
+        <div className='coin-container'>
+            <div className="coin-row">
+              <div className="coin">
+                <h1>Name</h1>
+                <p className="coin-symbol"></p>
+              </div>
+                <div className="coin-data">
+                    <p className="coin-price">Price</p>
+                    <p className="coin-volume">Volume</p>
+                     <p className="coin-percent red">Price Change</p>
+                    <p className="coin-marketcap">Market-cap</p>        
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Header
